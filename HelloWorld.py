c337a9c2ef78dc7160e114879e71e7e9af0ef926
JonathanOtubu <jfkstudiostv@gmail.com>
2022-08-02 15:56:45 +0100

    sort function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,15 @@
 def test():
-	print("hello world! changedddddd")
+	print("hello world! changed")
 	return
 test()
-
+aList = [3, 6, 1, 3, 1, 4, 1, 5, 6]
+bList = []
+def SortFromLowToHigh(aList):
+	for i in range(0, len(aList)):
+		
+			
+			bList.append(aList[i])
+			
+	return bList
+bList = SortFromLowToHigh(aList)
+print(bList)
