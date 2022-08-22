90ec432fa2076b4f3dffcbc7b672819872f1131d
JonathanOtubu <jfkstudiostv@gmail.com>
2022-08-22 19:07:26 +0100

    step 1
    separate numbers into pairs. eg 361 into 3 and 61

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,14 +2,49 @@ def test():
 	print("hello world! changed")
 	return
 test()
-aList = [3, 6, 1, 3, 1, 4, 1, 5, 6]
-bList = []
+
+def IsFromLowToHigh(aList):
+	for i in range(0,len(aList) -1):
+		element = aList[i]
+		nextElement = aList[i + 1]
+		if element > nextElement:
+			return 0
+	return 1
+
+aList = [1,0,0,0,0,0,0,0]
 def SortFromLowToHigh(aList):
-	for i in range(0, len(aList)):
-		
-			
-			bList.append(aList[i])
-			
+	while IsFromLowToHigh(aList) != 1:
+		for i in range(0,len(aList) - 1):
+			element = aList[i]
+			nextElement = aList[i+1]
+			if element > nextElement:
+				aList[i+1] = element
+				aList[i] = nextElement
+		#print(aList)
+	return aList
+aList = SortFromLowToHigh(aList)
+#print(aList)
+
+def IsFromHighToLow(bList):
+	for i in range(0,len(bList) -1):
+		element = bList[i]
+		nextElement = bList[i + 1]
+		if element < nextElement:
+			return 0
+	return 1
+
+
+bList = [5,3,6,7,3,2,8,7,2,1]
+def SortFromHighToLow(bList):
+	while IsFromHighToLow(bList) != 1:
+		for i in range(0,len(bList) - 1):
+			element = bList[i]
+			nextElement = bList[i+1]
+			if element < nextElement:
+				bList[i+1] = element
+				bList[i] = nextElement
+		print(bList)
 	return bList
-bList = SortFromLowToHigh(aList)
+bList = SortFromHighToLow(bList)
 print(bList)
+
---
+++ SquareRoot.py
@@ -0,0 +1,25 @@
+def SquareRootByLongDivision(a):
+#1. Separate your square root base into pairs. Starting from the right, group the number into pairs. 
+#In our example, 361 would be 3 61.
+
+	stringA = str(a)
+	listA = [c for c in stringA]
+	lenOfA = len(listA)
+	numOfModule = 10**lenOfA/10
+	remainder = a % numOfModule
+	firstDigit = (a - remainder) / numOfModule
+	print(firstDigit)
+
+
+	return firstDigit
+
+#2. Find the largest square that divides into the first number or pair. This will give you the first number in your answer. The first number on the left is 3. The highest square that goes into it is one because 1 x 1 = 1, while 2 x 2 = 4.
+
+#3. Subtract the square from the first number or pair. Subtracting the square from the first number will give you a remainder, which will be included in the next step. In this example, 3 - 1 = 2.
+#4. Drop down the next pair. The next number you will work with will be the combination of the subtracted square and the next pair. In this case, they would make a three-digit number. When you bring 61 down, the number you will use for finding the next digit in the square root is 261.
+#5. Multiply the first digit of the square by two. This will be the first digit in the factor for finding the second digit of the square root. In the example, the first digit of the square root is one. 1 x 2 = 2.
+#6. Set up the next factor equation. The equation for the next step is based on the digit from step five and the number from step four. The first factor will be a two-digit number, where the first digit is the number from step five. The equation will look like 2_ x _.
+#7. Find the number that fills the blanks. This number will be the next digit in the solution for √361. The number that will fill the blanks will be the same, and it will be the highest digit where the factors are less than or equal to the number in step four. In this example, the goal number is 261. We will start with 9, so the equation will look like 29 x 9 = 261.
+#8. Put the number next to the first digit. In this example, the square is 19.
+a = 361
+SquareRootByLongDivision(a)		
