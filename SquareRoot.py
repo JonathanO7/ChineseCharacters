a6c67118fbb2f3f26bf03b56a5d12f84dfbce7a5
JonathanOtubu <jfkstudiostv@gmail.com>
2022-09-10 12:46:25 +0100

    step 8

--- SquareRoot.py
+++ SquareRoot.py
@@ -30,11 +30,44 @@ def SquareRootByLongDivision(a):
 	#When you bring 61 down, the number you will use for finding the next digit in the square root is 261.
 	newNumber = str(subtractedFirstDigit) + str(remainder)
 	combinedNum = float(newNumber)
-	print(combinedNum)
+	
 
-	#5. Multiply the first digit of the square by two. This will be the first digit in the factor for finding the second digit of the square root. In the example, the first digit of the square root is one. 1 x 2 = 2.
-	#6. Set up the next factor equation. The equation for the next step is based on the digit from step five and the number from step four. The first factor will be a two-digit number, where the first digit is the number from step five. The equation will look like 2_ x _.
-	#7. Find the number that fills the blanks. This number will be the next digit in the solution for √361. The number that will fill the blanks will be the same, and it will be the highest digit where the factors are less than or equal to the number in step four. In this example, the goal number is 261. We will start with 9, so the equation will look like 29 x 9 = 261.
-	#8. Put the number next to the first digit. In this example, the square is 19.
+	'''
+	5. Multiply the first digit of the square by two. 
+	This will be the first digit in the factor for finding the second digit of the square root.
+	In the example, the first digit of the square root is one. 1 x 2 = 2.
+	'''
+	firstDigitx2 = firstDigit*2
+
+	'''
+	6. Set up the next factor equation(No need to do anything here). 
+	The equation for the next step is based on the digit from step five and the number from step four. 
+	The first factor will be a two-digit number, where the first digit is the number from step five. 
+	The equation will look like 2_ x _.
+	'''
+	
+
+	'''
+	7. Find the number that fills the blanks. 
+	This number will be the next digit in the solution for √361. 
+	The number that will fill the blanks will be the same, 
+	and it will be the highest digit where the factors are less than or equal to the number in step four. 
+	In this example, the goal number is 261. We will start with 9, so the equation will look like 29 x 9 = 261.
+
+	'''
+
+	for i in range(0,int(combinedNum + 1)):
+		firstFactor = str(firstDigitx2) + str(i)
+		firstFactor = float(firstFactor)
+		if firstFactor*i == combinedNum:
+			#8. Put the number next to the first digit. In this example, the square is 19.
+			finalAnswer = str(firstDigit) + str(i)
+			finalAnswer = float(finalAnswer)
+			print(finalAnswer)
+
+			return finalAnswer
+
+	
 a = 361
-SquareRootByLongDivision(a)		
+finalAnswer = SquareRootByLongDivision(a)
+print(finalAnswer)		
