dfaf282c4d86e996fee6e8fe588754b90ca7c235
JonathanOtubu <jfkstudiostv@gmail.com>
2022-10-03 19:02:09 +0100

    Update CubeRoot.py

--- CubeRoot.py
+++ CubeRoot.py
@@ -1,5 +1,5 @@
 print("Please Enter A Number")
-a = 0.1
+a = -0.1
 
 
 def cbrt(a):
@@ -10,17 +10,27 @@ def cbrt(a):
 	if type(a) == str:
 		print("Please enter a number")
 		return None
+
+	
 	a = float(a)
 	threshold = 0.000001
 	lowerlimit = 0
 	upperlimit = a
+
+	if 0 < a < 1:
+			upperlimit = 1
+
+	if -1 < a < 0:
+			upperlimit = -1
+
+	
 	if a > 2**63:
 			print("number is too large")
 			return None
-	initialguess = lowerlimit + (upperlimit - lowerlimit)/2
-
-	error = abs(initialguess**3 - a)
+	initialguess = lowerlimit + (upperlimit - lowerlimit)/2       
 	
+	error = abs(initialguess**3 - a)
+
 	count = 0
 	while error > threshold:
 		
@@ -28,6 +38,7 @@ def cbrt(a):
 
 		if count > 5000:
 			return initialguess
+			
 		if a < 0:
 
 			if initialguess**3 > a:
@@ -41,10 +52,15 @@ def cbrt(a):
 				upperlimit = initialguess
 			else:
 				lowerlimit = initialguess
+
 		initialguess = lowerlimit + (upperlimit - lowerlimit)/2
 		error = abs(initialguess**3 - a)
+	
 		if error < threshold:
+			if -1 < a < 0:
+				initialguess = initialguess*-1
 			return initialguess
+
 
 cbrt = cbrt(a)
 print(cbrt)
